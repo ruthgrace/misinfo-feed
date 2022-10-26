9e124a8d8eddff261d2c9712e840a853744335b7
Ruth Grace Wong <ruthgracewong@gmail.com>
2022-10-26 05:48:43 +0000

    robin got it to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import React, { useEffect, useState} from 'react';
 import './App.css'
+import { read, FeedEntry } from 'feed-reader'
+
+const corsProxy = "https://cors-anywhere.robinpham1.repl.co"
+const url = `${corsProxy}/factcheck.org/feed/`
 
 export default function App() {
+  const [posts, setPosts] = useState<FeedEntry[]>()
+  
+  React.useEffect(() => {
+    read(url).then(res => {
+      console.log('result', res);
+      setPosts(res.entries)
+    }).catch(e => console.error(e))
+  }, [])
+
+  console.log('Posts', posts)
+  console.log('test code mobile')
+
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-      hello world one two three
+      <code>
+        {JSON.stringify(posts ?? [], null, 2)}
+      </code>
     </main>
   )
 }
