d2f94612f3528b936f42ce84cfecde01ed97a312
Adam McKenna <adam@flounder.net>
2023-03-22 05:18:14 +0000

    Update build scripts

--- docker/build.sh
+++ docker/build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-BUILD="./build"
+set -e
 
-git submodule update
+BUILD="$(pwd)/build"
+APP="app"
+BACKEND="backend"
 
-rm -rf "$BUILD" && mkdir -p "$BUILD" && pushd "$BUILD"
+git submodule update --init
+
+rm -rf "$BUILD" && mkdir -p "$BUILD" "$BUILD/$APP" "$BUILD/$BACKEND" && cd "$BUILD"
 
 cp ../docker-node/19/bullseye/* .
-docker build --no-cache -t node-misinfo .
-popd
-pushd app
-docker build --no-cache -t misinfo-app .
-popd
-pushd backend
-docker build --no-cache -t misinfo-backend .
 
+docker build -t node-misinfo .
+
+cp -a ../../public "$BUILD/$APP"
+cp -a ../../src "$BUILD/$APP"
+cp -a ../../backend/app.js "$BUILD/$BACKEND"
+cp -a ../app/Dockerfile app
+cp -a ../backend/Dockerfile backend
+
+docker build --no-cache -t misinfo-app app
+docker build --no-cache -t misinfo-backend backend
 
