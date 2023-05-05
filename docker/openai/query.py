ab9e0e08fc1a6519ac3b243473e8e80ca5733c43
Adam McKenna <adam@flounder.net>
2023-05-05 17:21:43 +0000

    Add options, code to check openai

--- docker/openai/query.py
+++ docker/openai/query.py
@@ -1,18 +1,37 @@
-import openai
 import os
+import openai
 import argparse
 from testcases import TESTCASES
 import random
 import colorama
+from dotenv import load_dotenv
+import logging
+import sys
 
 colorama.init(autoreset=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', dest='prompt', type=str, required=False)
 parser.add_argument('-a', dest='a', action='store_true', required=False)
+parser.add_argument('-v', dest='verbose', action='store_true', default=False)
 args = parser.parse_args()
 
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+if args.verbose:
+    logger.setLevel(logging.DEBUG)
+    handler.setLevel(logging.DEBUG)
+
+logger.addHandler(handler)
+
+
 # Set up your OpenAI API key
+load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 expected_response = None
 
@@ -38,7 +57,7 @@ def call_chatgpt(conversation):
     )
 
 def gen_request(user_input, expected_response):
-    print(f'Checking prompt: {user_input}, expected response is {expected_response}')
+    logger.info(f'Checking prompt: {user_input}, expected response is {expected_response}')
 
     user_prompt = f'Is a news headline "{user_input}" related to health, health care, disease, or public health in any way? Please answer "Yes" or "No". If you are unsure, say "Unsure".'
     #user_prompt = f'Does the phrase "{user_input}" relate to health care in any way?'
@@ -52,37 +71,39 @@ def gen_request(user_input, expected_response):
 def check_response(response, expected_response):
     if expected_response is not None:
         response_bool = strtobool(response)
-        if not response_bool:
-            print(colorama.Fore.YELLOW + '[UNSURE]: AI did not return a definitive answer')
+        if response_bool is None:
+            logger.info(colorama.Fore.YELLOW + '[UNSURE]: AI did not return a definitive answer')
             return
-        if expected_response == response_bool:
-            print(colorama.Fore.GREEN + '[SUCCESS]: Reply matched expected response!')
+        elif expected_response == response_bool:
+            logger.info(colorama.Fore.GREEN + '[SUCCESS]: Reply matched expected response!')
             return True
         else:
-            print(colorama.Fore.RED + '[FAILURE]: Reply did not match expected response!')
+            logger.info(colorama.Fore.RED + '[FAILURE]: Reply did not match expected response!')
             return False
 
 def output_response(response, expected_response):
     assistant_reply = response['choices'][0]['message']['content']
 
-    print("ChatGPT: " + assistant_reply)
+    logger.info("ChatGPT: " + assistant_reply)
     return check_response(assistant_reply, expected_response)
     
+def main():
+    if args.prompt:
+        logger.info(f'Checking prompt: {args.prompt}')
+        check_prompt(args.prompt, None)
+    elif args.a:
+        logger.info('Checking all testcases')
+        failures = 0
+        total = len(TESTCASES)
+        for testdata in TESTCASES:
+            # 'Unsure' is currently considered a failure
+            if not check_prompt(*testdata):
+                failures += 1
+        logger.info(f'Success Rate: [{total - failures}/{total}] ({(total - failures) / total * 100}%)')
+    else:
+        testdata = random.choice(TESTCASES)
+        check_prompt(*testdata)
 
-if args.prompt:
-    print(f'Checking prompt: {args.prompt}')
-    check_prompt(args.prompt, None)
-elif args.a:
-    print('Checking all testcases')
-    failures = 0
-    total = len(TESTCASES)
-    for testdata in TESTCASES:
-        # 'Unsure' is currently considered a failure
-        if not check_prompt(*testdata):
-            failures += 1
-    print(f'Success Rate: [{total - failures}/{total}] ({(total - failures) / total * 100}%)')
 
-else:
-    testdata = random.choice(TESTCASES)
-    check_prompt(*testdata)
-
+if __name__ == "__main__":
+    main()
