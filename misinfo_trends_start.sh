820820b9c4f8e646f88730062c41b018579589b8
Ruth Grace Wong <ruthgracewong@gmail.com>
2023-06-01 20:06:25 -0700

    Update misinfo_trends_start.sh
    fix option in wrong place

--- misinfo_trends_start.sh
+++ misinfo_trends_start.sh
@@ -2,4 +2,4 @@
 
 docker stop misinfo-app-prod
 docker rm misinfo-app-prod
-docker --net=host run -e PRODUCTION=true --name misinfo-app-prod misinfo-app:latest
+docker run -e --net=host PRODUCTION=true --name misinfo-app-prod misinfo-app:latest
