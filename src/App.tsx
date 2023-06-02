f249bffe9ac33087f0236af89bf6dca85060197d
Adam McKenna <adam@flounder.net>
2023-06-02 05:21:32 +0000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,33 @@ import './App.css'
 import { read, FeedEntry } from 'feed-reader'
 import { fetch } from 'fetch-opengraph';
 import { rssConfigs } from './const';
+import { createClient } from '@supabase/supabase-js'
+
+let SUPABASE_ANON_KEY: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhqb2JxbHVldnF0dmpod2ZobGhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4NDA3NjEsImV4cCI6MTk5NDQxNjc2MX0.7HCav4pUOTSEVaqDKv7vCRxApIhqecs_OfYB8_GDqto"
+let SUPABASE_URL: string = "https://db.xjobqluevqtvjhwfhlhr.supabase.co/"
+
+
+interface Database {
+  public: {
+    Tables: {
+      feeditem: {
+        Row: {} // The data expected to be returned from a "select" statement.
+        Insert: {} // The data expected passed to an "insert" statement.
+        Update: {} // The data expected passed to an "update" statement.
+      }
+    }
+  }
+}
+
+const supabase = createClient<Database>(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+
+type FeedItemResponse = Awaited<ReturnType<typeof getFeedItems>>;
+export type FeedItemResponseSuccess = FeedItemResponse['data'];
+export type FeedItemResponseError = FeedItemResponse['error'];
 
 interface FeedItem {
   timestamp?: Date;
@@ -39,21 +66,29 @@ function useGetFeed(key: string) {
   return feedItems;
 }
 
+async function getFeedItems() {
+  let now: Date  = new Date();
+  let then: Date = new Date();
+  then.setDate(now.getDate() - 1);
+  return await supabase.from('feeditem').select(`timestamp, uri, title, description, feed (id)`).eq('public_health_related', 1).gte('timestamp',now.toISOString()).lte('timestamp',then.toISOString());
+}
+
 
 export default function App() {
-  let allPosts: FeedItem[] = [
-    ...useGetFeed('fco'),
-    ...useGetFeed('ls'),
-    ...useGetFeed('pf'),
-    ...useGetFeed('sf'),
-    ...useGetFeed('dp'),
-    ...useGetFeed('afp'),
-    ...useGetFeed('apn'),
-    ...useGetFeed('ust'),
-    ...useGetFeed('rfc'),
+  getFeedItems().then(data => console.log('FeedItems', data));
 
-  ]
-  console.log('allPosts', allPosts)
+  let allPosts: FeedItem[] = [ ];
+    //...useGetFeed('fco'),
+    //...useGetFeed('ls'),
+    //...useGetFeed('pf'),
+    //...useGetFeed('sf'),
+    //...useGetFeed('dp'),
+    //...useGetFeed('afp'),
+    //...useGetFeed('apn'),
+    //...useGetFeed('ust'),
+    //...useGetFeed('rfc'),
+  //]
+  //console.log('allPosts', allPosts)
 
   allPosts = allPosts.sort((n1, n2) => (n2.timestamp?.getTime() ?? 0) - (n1.timestamp?.getTime() ?? 0));
 
