fadde3e20c16566cc751cad27ca531270d66941f
Adam McKenna <adam@flounder.net>
2023-06-28 01:33:27 +0000

    Add thumbnail scraping from feed data

--- docker/fetch-new-articles/pull_rss_feeds.py
+++ docker/fetch-new-articles/pull_rss_feeds.py
@@ -3,10 +3,13 @@ import time
 import feedparser
 
 from rss_feed import rssFeed
-from query import check_prompt, args
+from query import check_prompt
+from cli_args import args
 from my_logger import logger
 from dotenv import load_dotenv
 from supabase import create_client, Client
+from bs4 import BeautifulSoup
+
 
 DB_DATABASE_NAME = "postgres"
 DB_PORT = 5432
@@ -23,9 +26,24 @@ def item_exists_in_db(supabase: Client, uri: str) -> bool:
         return True
     return False
 
+def get_thumbnail_url(entry):
+    # first check for media_thumbnail in entry keys
+    if 'media_thumbmail' in entry.keys():
+        return entry.media_thumbnail[0].get('url')
+    elif 'media_content' in entry.keys():
+        return entry.media_content[0].get('url')
+    else:
+        # if we can't find it, parse entry.content
+        try:
+            soup = BeautifulSoup(entry.content[0].value, 'lxml')
+            thumbnail = soup.find_all('img')[0].get('src')
+            return thumbnail
+        except (KeyError, AttributeError):
+            return
 
-def add_entries_to_feed_table(supabase: Client, entries: list, force_update: bool = False) -> None:
-    logger.debug(f'Adding {len(entries)} entries to feed table...')
+
+def add_entries_to_feed_table(supabase: Client, entries: list) -> None:
+    logger.info(f'Adding {len(entries)} entries to feed table...')
     insert_list = []
     for entry in entries:
         value = {'rss_uri': entry.get('url'), 'logo': entry.get('logo'), 'website': entry.get('website'), 'title': entry.get('title')}
@@ -33,12 +51,17 @@ def add_entries_to_feed_table(supabase: Client, entries: list, force_update: boo
     data = supabase.table('feed').insert(insert_list).execute()
 
 
-def add_entries_to_feeditem_table(supabase: Client, feed_id: int, entries: list, force_update: bool = False) -> None:
-    logger.debug(f'Adding {len(entries)} entries to feeditem table...')
+def add_entries_to_feeditem_table(supabase: Client, feed_id: int, entries: list) -> None:
+    logger.info(f'Adding {len(entries)} entries to feeditem table...')
     insert_list = []
     for entry in entries:
+        try:
+            thumbnail_url = get_thumbnail_url(entry)
+        except Exception as e:
+            print(e)
+            breakpoint()
         uri = entry.get('links')[0].get('href','')
-        value = {'uri': uri, 'title': entry.title, 'feed_id': feed_id}
+        value = {'uri': uri, 'title': entry.title, 'feed_id': feed_id, 'thumbnail_url': thumbnail_url}
         insert_list.append(value)
     supabase.table('feeditem').upsert(insert_list, on_conflict='uri').execute()
 
@@ -65,6 +88,7 @@ def main():
     for entry in master_feed_list:
         logger.info(f'Checking url: {entry["rss_uri"]}')
         feed = rssFeed(feedparser.parse(entry["rss_uri"]))
+        #breakpoint()
         logger.info(f'found {len(feed.entries)} items')
         health_related_articles = []
         for item in feed.entries:
@@ -76,7 +100,7 @@ def main():
                     logger.info(f'found healthcare related article: title: {item.title}, description: {item.description}')
                     health_related_articles.append(item)
         print(f'found {len(health_related_articles)} articles related to healthcare on {entry["website"]}')
-        add_entries_to_feeditem_table(supabase, entry.get('id'), health_related_articles, args.force_update)
+        add_entries_to_feeditem_table(supabase, entry.get('id'), health_related_articles)
         
 
 if __name__ == "__main__":
