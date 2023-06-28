94cbc6d08cdcbb19dc82a104b82aec201f464d18
Adam McKenna <adam@flounder.net>
2023-06-28 01:33:46 +0000

    Move argparse to its own file

--- docker/fetch-new-articles/query.py
+++ docker/fetch-new-articles/query.py
@@ -6,22 +6,11 @@ from testcases import TESTCASES
 import random
 import colorama
 from dotenv import load_dotenv
-from my_logger import logger, set_level
-import logging
+from my_logger import logger
+from cli_args import args
 import sys
 
 colorama.init(autoreset=True)
-DEFAULT_LOGLEVEL = logging.ERROR
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', dest='prompt', type=str, required=False)
-parser.add_argument('-a', dest='a', help='Check all testcase responses', action='store_true', required=False)
-parser.add_argument('-v', dest='verbose', help='log verbosity, default ERROR, -v = INFO, -vv = DEBUG', action='count', default=0)
-parser.add_argument('--force-update', dest='force_update', help='Add/update all articles from feeds to db regardless of seen status', action='store_true')
-args = parser.parse_args()
-
-if args.verbose:
-    set_level(40 - (args.verbose * 10))
 
 
 # Set up your OpenAI API key
