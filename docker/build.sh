d0eb860abd3686c36369d08f976d3d372585b77f
Ruth Grace Wong <ruthgracewong@gmail.com>
2023-06-27 19:03:12 -0700

    Update build.sh - copy hidden files including .env

--- docker/build.sh
+++ docker/build.sh
@@ -11,11 +11,10 @@ fi
 BUILD="$(pwd)/build"
 APP="app"
 BACKEND="backend"
-FETCH_NEW="fetch-new-articles"
 
 git submodule update --init
 
-rm -rf "$BUILD" && mkdir -p "$BUILD" "$BUILD/$APP" "$BUILD/$BACKEND" "$BUILD/$FETCH_NEW" && cd "$BUILD"
+rm -rf "$BUILD" && mkdir -p "$BUILD" "$BUILD/$APP" "$BUILD/$BACKEND" && cd "$BUILD"
 
 cp ../docker-node/19/bullseye/* .
 
@@ -39,7 +38,7 @@ cp -a ../../package.json "$BUILD/$BACKEND"
 cp -a ../../tsconfig.json "$BUILD/$BACKEND"
 cp -a ../app/Dockerfile app
 cp -a ../backend/Dockerfile backend
-cp -a ../fetch-new-articles/* "$FETCH_NEW"
+cp -a ../fetch-new-articles "$BUILD/"
 
 docker build --no-cache -t misinfo-app app
 # docker build --no-cache -t misinfo-backend backend
